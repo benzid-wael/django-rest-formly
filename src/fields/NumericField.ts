53f3c067ac7473b6125abe174af1f963fb4e1c64
Wael BEN ZID <benzid.wael@hotmail.fr>
2015-12-09 10:37:23 +0100

    Base implementation for NumericField

---
+++ src/fields/NumericField.ts
@@ -0,0 +1,41 @@
+/// <reference path="../_all.d.ts" />
+"use strict";
+
+import base = require('./base');
+import utils = require('../utils');
+
+
+export interface INumericField extends base.IField {
+
+  /**
+   * The minimum value.
+   */
+  minValue?: number;
+
+  /**
+   * The maximum value.
+   */
+  maxValue?: number;
+}
+
+
+export class NumericField extends base.Field implements INumericField {
+
+  protected static fieldType: string = 'input';
+  protected static templateType: string = 'number';
+  minValue: number;
+  maxValue: number;
+
+  constructor(options: base.IDjangoRestFieldOptions) {
+    this.minValue = options.min_value;
+    this.minValue = options.max_value;
+    super(options);
+  }
+
+  protected getExtraTemplateOptions() {
+    return utils.smartExtend({}, {
+      min: this.minValue,
+      max: this.maxValue
+    });
+  }
+}

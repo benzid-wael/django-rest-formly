8382e462d0ac2d6a33954fbb680005372c4083f9
Wael BEN ZID <benzid.wael@hotmail.fr>
2015-12-09 10:29:52 +0100

    Minor changes

--- src/_all.d.ts
+++ src/_all.d.ts
@@ -3,4 +3,6 @@
 /// <reference path="./main.ts" />
 /// <reference path="./utils.ts" />
 /// <reference path="./fields/base.ts" />
+/// <reference path="./fields/CharField.ts" />
+/// <reference path="./fields/NumericField.ts" />
 
--- src/fields/base.ts
+++ src/fields/base.ts
@@ -1,4 +1,4 @@
-/// <reference path="../_all.ts" />
+/// <reference path="../_all.d.ts" />
 
 import AngularFormly = require('angular-formly');
 

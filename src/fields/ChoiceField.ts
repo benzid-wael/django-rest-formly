d3e81721ff3221a56c3f8644e1d718cef1e08c6b
WAEL BEN ZID <benzid.wael@hotmail.fr>
2015-12-15 17:33:56 +0100

    BUGFIX Django REST representation of choices is different than angular-formly one

--- src/fields/ChoiceField.ts
+++ src/fields/ChoiceField.ts
@@ -13,7 +13,7 @@ export interface IChoiceField extends base.IField {
   /**
    * List of accepted values.
    */
-  choices: Array<interfaces.IOption>;
+  choices: Array<interfaces.IDjangoRestOption>;
 }
 
 
@@ -23,16 +23,16 @@ export interface IChoiceField extends base.IField {
 export abstract class ChoiceField extends base.Field implements IChoiceField, base.IField {
 
   protected static templateType: string = null;
-  choices: Array<interfaces.IOption>;
+  choices: Array<interfaces.IDjangoRestOption>;
 
   constructor(options: interfaces.IDjangoRestFieldOptions) {
-    this.choices = <Array<interfaces.IOption>> options.choices;
+    this.choices = <Array<interfaces.IDjangoRestOption>> options.choices;
     super(options);
   }
 
   protected getExtraTemplateOptions() {
-    var res     = {options: null},
-        options = [];
+    var res: {options?: Array<interfaces.IOption>} = {},
+        options: Array<interfaces.IOption>  = [];
     if (!this.readOnly && this.choices) {
       this.choices.forEach(function (choice) {
         options.push({

9cf392bf022f5bf82b5126925907c67af522506b
WAEL BEN ZID <benzid.wael@hotmail.fr>
2015-12-16 19:18:38 +0100

    Generalize bump task

--- Gulpfile.js
+++ Gulpfile.js
@@ -126,24 +126,43 @@ gulp.task("typedoc", function() {
 
 gulp.task('bump', function() {
 
-  var questions = [
-    {
+  var questions = [{
       type: 'input',
       name: 'bump',
       message: 'Are you sure you want to bump the patch version? [Y/N]'
-    }
-  ];
+  }], versionQuestions = [{
+      type: 'list',
+      name: "type",
+      message: "Semver version type to bump?",
+      choices: [
+        "major",
+        "minor",
+        "patch",
+        "prerelease"
+      ],
+      default: "patch"
+  }, {
+      type: 'input',
+      name: "preid",
+      message: "Set the prerelase tag to use, e.g: [alpha|beta|rc]"
+  }];
+
 
   inquirer.prompt( questions, function( answers ) {
     if(answers.bump === 'Y') {
+      inquirer.prompt( versionQuestions, function( answers ) {
+          var semverConfig = {type : answers.type};
+          if (semverConfig.type == 'prerelease') {
+              semverConfig.preid = answers.preid;
+          }
 
-      return gulp.src(['./package.json', './bower.json'])
-          .pipe(bump({type: 'patch'}))
-          .pipe(gulp.dest('./'))
-          .pipe(git.commit('bump patch version'))
-          .pipe(filter('package.json'))  // read package.json for the new version
-          .pipe(tagVersion());           // create tag
-
+          return gulp.src(['./package.json', './bower.json'])
+              .pipe(bump(semverConfig))
+              .pipe(gulp.dest('./'))
+              //.pipe(git.commit('bump patch version'))
+              //.pipe(filter('package.json'))  // read package.json for the new version
+              //.pipe(tagVersion());           // create tag
+      });
     }
   });
 });

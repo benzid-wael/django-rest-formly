348cd5f91d475e6f1d1865782135e0b11fa88d0c
WAEL BEN ZID <benzid.wael@hotmail.fr>
2015-12-16 11:21:37 +0100

    Force compiling typescript files before running tests and update tsd dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,36 @@
  * @preferred
  */
 
-export class Converter {
+import AngularFormly = require('angular-formly');
 
+import {IDjangoRestFieldOptions} from "./interfaces";
+import {Field} from "./fields/base";
+import {DjangoRestConfig, IFieldFactory} from "./DjangoRestConfig";
+
+
+export class Converter {
+  fields: Array<Field>;
+
+  constructor (djangoRestMeta: any, fieldFactoryFn: IFieldFactory) {
+    this.fields = [];
+    for (var fieldName in djangoRestMeta) {
+      var fieldConfig = djangoRestMeta[fieldName];
+      fieldConfig.name = fieldName;
+      this.fields.push(DjangoRestConfig.factory(<IDjangoRestFieldOptions>fieldConfig, fieldFactoryFn));
+    };
+  }
+
+  public convert() {
+    var configObjects: Array<AngularFormly.IFieldConfigurationObject> = [];
+    this.fields.forEach(function(field) {
+      configObjects.push(field.getConfigurationObject());
+    });
+    return configObjects;
+  };
+}
+
+
+export function DjangoRestFrameworkAdapter(djangoRestMeta: Array<IDjangoRestFieldOptions>, fieldFactoryFn?: IFieldFactory) {
+  var converter = new Converter(djangoRestMeta, fieldFactoryFn);
+  return converter.convert();
 }
--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,6 +1,6 @@
 
 /// <reference path="angularjs/angular.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
 /// <reference path="angular-formly/angular-formly.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
 /// <reference path="chai/chai.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />

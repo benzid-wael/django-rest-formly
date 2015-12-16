9b546c636ca66b971fa47c27637423648df05e90
WAEL BEN ZID <benzid.wael@hotmail.fr>
2015-12-16 16:16:35 +0100

    tsd dependencies updated

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,6 +1,7 @@
 
-/// <reference path="angularjs/angular.d.ts" />
 /// <reference path="angular-formly/angular-formly.d.ts" />
 /// <reference path="mocha/mocha.d.ts" />
 /// <reference path="chai/chai.d.ts" />
 /// <reference path="underscore/underscore.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />

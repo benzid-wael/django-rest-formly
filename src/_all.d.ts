e7e11f4b718642474be87070d7310874197b9814
WAEL BEN ZID <benzid.wael@hotmail.fr>
2015-12-22 12:41:40 +0100

    Improve structure

--- src/_all.d.ts
+++ src/_all.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+/// <reference path="./contrib/prettyjson.d.ts" />
+
 /// <reference path="./interfaces.ts" />
 /// <reference path="./main.ts" />
 /// <reference path="./utils.ts" />
@@ -9,8 +11,5 @@
 /// <reference path="./fields/CharField.ts" />
 /// <reference path="./fields/NumericField.ts" />
 /// <reference path="./fields/ChoiceField.ts" />
-
-/// <reference path="./prettyjson.d.ts" />
-
 
 
